e13a7b4db863cb1b9e79e17d378e9cb0f59c1d72
PiperAtDawn <master_zayats@mail.ru>
2022-02-02 16:30:05 +0300

    Add bubble sort method

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,1 +1,18 @@
+def bubble_sort (array)
+  new_array = array.map {|e| e.dup}
+  sorted = false
+  until sorted do
+    sorted = true
+    for i in 0..new_array.length - 2 do
+      if new_array[i] > new_array[i+1]
+        sorted = false
+        new_array[i], new_array[i+1] = new_array[i+1], new_array[i]
+      end
+    end
+  end
+  new_array
+end
 
+array = [3, 4, 1, 0, 5, 6, 10, 12, 11]
+
+p bubble_sort(array)
